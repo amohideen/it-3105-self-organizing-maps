f73f7fd84af8cf7b3b60a6e99325ae7975b639ef
Espen Meidell <espen.meidell@gmail.com>
2017-11-17 10:24:54 +0100

    Added random search

--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ from DataReader import DataReader
 from Utilities import Utilities
 from SOM import SOM
 import cProfile
+import random
+import threading
 
 NoOp = None
 
@@ -89,11 +91,57 @@ def run_tsm(city: int, visualize: bool=True, profile: bool = False):
     Utilities.make_gif(mnist=False) if visualize else NoOp
 
 
+def random_search_tsm(city: int):
+    cities = DataReader.read_tsm_file(city)
+    norm_cities = Utilities.normalize_coordinates(cities)
+    features = norm_cities[:, 1:]
+
+    while True:
+        node_factor = random.randint(2, 10)
+        radius_divisor = random.uniform(1, 3)
+        n_epochs = 400
+        l_rate = random.random()
+        funcs = ["exp","power","linear"]
+        r_decay = random.choice(funcs)
+        l_decay = random.choice(funcs)
+
+        out_size = len(features) * node_factor
+        init_rad = int(out_size / radius_divisor)
+
+        som = SOM(mnist=False,
+                  features=features,
+                  n_epochs=n_epochs,
+                  n_output_rows=1,
+                  n_output_cols=out_size,
+                  initial_radius=init_rad,
+                  initial_l_rate=l_rate,
+                  radius_decay_func=r_decay,
+                  l_rate_decay_func=l_decay,
+                  originals=cities[:, 1:],
+                  display_interval=-1)
+
+        Utilities.store_tsm_result(case=city,
+                                   epochs=n_epochs,
+                                   nodes=node_factor,
+                                   l_rate=l_rate,
+                                   radius=radius_divisor,
+                                   l_decay=l_decay,
+                                   r_decay=r_decay,
+                                   result=som.run())
+
+
+def random_search_multithread(city: int, threads: int):
+    for i in range(threads):
+        t = threading.Thread(target=random_search_tsm, args=(city, ))
+        t.start()
+
+
 def main(mnist: bool, profile: bool=False):
     if mnist:
         run_mnist(profile=profile)
     else:
-        run_tsm(city=8, visualize=False, profile=profile)
+        # run_tsm(city=8, visualize=False, profile=profile)
+        random_search_multithread(8, 3)
 
 
 if __name__ == "__main__":
